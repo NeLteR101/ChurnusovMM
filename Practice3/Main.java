99798d87c5da45fbe9f3669244f41cbc390f22b9
NeLteR101 <92322307+NeLteR101@users.noreply.github.com>
2022-07-01 07:59:14 +0300

    Create Main.java

---
+++ Practice3/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    Commodity apple = new Commodity(20061, 12312, "Яблоко", 20, 30, "Зеленые яблоки");
+    System.out.println(apple);
+    }
+}
